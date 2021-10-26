2b6307873597f57686e0ac626da81436016bd502
u900333 <TMelendez@proveedor.teco.com.ar>
2021-10-26 09:57:02 -0300

    Section 3 complete

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,1 +1,7 @@
-exports.createTodo = () => {}
+const TodoModel = require("../model/todo.model")
+
+
+exports.createTodo = (req, res, next) => {
+    const createModel = TodoModel.create(req.body)
+    res.status(201).json(createModel)
+}
